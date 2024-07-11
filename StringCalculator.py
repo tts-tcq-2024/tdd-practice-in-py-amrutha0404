8a63680ab9ce4337bfbf89bac44023242ae77905
amrutha0404 <99201771+amrutha0404@users.noreply.github.com>
2024-07-11 10:10:52 +0530

    Updated StringCalculator.py to handle numbers >1000

--- StringCalculator.py
+++ StringCalculator.py
@@ -1,10 +1,21 @@
-def add(input):
-  if is_empty_string(input):
-    return 0
-  return sum_numbers(input.split(","))
+import re
 
-def is_empty_string(input):
-  return input == ""
+def add(input):
+    if is_empty_string(input):
+        return 0
+    numbers = filter_numbers(input)
+    return sum_numbers(numbers)
+
+def filter_numbers(input):
+  numbers = re.findall(r'-?\d+', input)
+  return numbers
+  
+def is_empty_string(number_string):
+    return number_string == ""
 
 def sum_numbers(numbers):
-    return sum(map(int, numbers))
+    number_list = []
+    for number in numbers:
+        if int(number) < 1000:
+            number_list.append(int(number))
+    return sum(number_list)
